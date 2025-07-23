d2d063a09871c03aa0e94f2aed58619fe2b653e5
Aditya-kumar2005 <nanuadityakumar@gmail.com>
2025-07-24 00:10:23 +0530

    My java project Simple Banking System

---
+++ Simplebankingsystem/src/main/java/com/example/simplebankingsystem/Simplebankingsystem.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.example.simplebankingsystem;
+
+/**
+ *
+ * @author nanua
+ */
+public class Simplebankingsystem {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Simplebankingsystem/src/main/java/com/example/simplebankingsystem/connec.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.simplebankingsystem;
+
+import java.sql.*;
+
+/**
+ *
+ * @author nanua
+ */
+public class connec {
+    Connection c;
+    Statement s;
+    public connec(){
+        try{
+           String url="jdbc:mysql://localhost:3306/mysql?zeroDateTimeBehavior=CONVERT_TO_NULL";
+           c=DriverManager.getConnection(url,"root","Adity@25062005"); 
+        }
+        catch(Exception e){
+           
+        }
+    }
+}
